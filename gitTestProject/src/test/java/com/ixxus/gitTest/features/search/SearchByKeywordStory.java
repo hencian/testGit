91ebc506193896a9b9b74270f50310c77c5d8660
hencian <horatiu.encian@ixxus.com>
2016-06-30 11:09:11 +0300

    test cahnaged

--- gitTestProject/src/test/java/com/ixxus/gitTest/features/search/SearchByKeywordStory.java
+++ gitTestProject/src/test/java/com/ixxus/gitTest/features/search/SearchByKeywordStory.java
@@ -22,7 +22,6 @@ public class SearchByKeywordStory {
     @Steps
     public EndUserSteps anna;
 
-    @Issue("#WIKI-1")
     @Test
     public void searching_by_keyword_apple_should_display_the_corresponding_article() {
         anna.is_the_home_page();
@@ -36,8 +35,10 @@ public class SearchByKeywordStory {
         anna.is_the_home_page();
         anna.looks_for("pear");
         anna.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
+        
     }
 
+    //this test will not run
     @Pending @Test
     public void searching_by_ambiguious_keyword_should_display_the_disambiguation_page() {
     }
